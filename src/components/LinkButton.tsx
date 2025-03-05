926b19362cdf51982eb5341d7e022de58222da27
munergenis <munergenis@gmail.com>
2025-03-05 13:36:46 +0100

    fix: use of className function to merge styles

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils";
 import type { ReactNode } from "react";
 
 interface Props {
@@ -9,7 +10,8 @@ interface Props {
 const LinkButton = ({ href, children, className = "" }: Props) => {
   return (
     <a
-      className={`bg-white p-2 rounded-sm text-black font-semibold ${className}`}
+      // className={`bg-white p-2 rounded-sm text-black font-semibold ${className}`}
+      className={cn("font-semibold hover:underline", className)}
       href={href}
       target="_blank"
     >
